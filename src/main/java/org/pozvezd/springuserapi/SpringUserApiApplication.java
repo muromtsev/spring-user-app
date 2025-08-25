1a874696c5b8b7d6869fd819452e302d5f1c0b9c
muromtsev <t4k.org@mail.ru>
2025-08-25 18:39:26 +0300

    initial project

---
+++ src/main/java/org/pozvezd/springuserapi/SpringUserApiApplication.java
@@ -0,0 +1,13 @@
+package org.pozvezd.springuserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pozvezd/springuserapi/SpringUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pozvezd.springuserapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
